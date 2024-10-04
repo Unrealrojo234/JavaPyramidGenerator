62f2ac68decda9e236c2eaec407fa6b8a205482a
Unrealrojo234 <otienoryan812@gmail.com>
2024-10-04 18:31:50 +0300

    Enabled user input

--- Main.java
+++ Main.java
@@ -1,13 +1,39 @@
+import java.io.*;
+import java.text.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        Make();
+        int size;
+
+        System.out.println("Enter the pyramid size e.g 10");
+
+        try{
+            InputStreamReader isr = new InputStreamReader(System.in);
+
+            BufferedReader b = new BufferedReader(isr);
+
+            String sizeStr = b.readLine();
+
+            DecimalFormat df  = new DecimalFormat();
+
+            Number x = df.parse(sizeStr);
+            
+            size = x.intValue();
+            
+        }catch(IOException | ParseException e){
+            size = 10;
+        }
+        
+        Make(size);
+
 
     }
     
-    static void Make(){
-        for(int i = 0; i<=10;i++)
+    static void Make(int x){
+        
+        for(int i = 0; i<=x;i++)
         {   
             String pyramid;
 
